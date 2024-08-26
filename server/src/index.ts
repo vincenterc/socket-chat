4a48defae46a964fcf59e2b80a4de500406c3454
vincenter <vincenterr@yahoo.com.tw>
2024-08-26 19:17:04 +0800

    Initialize the server

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const ClientURL = 'http://localhost:3001'
+
+const server = createServer()
+const io = new Server(server, {
+  cors: {
+    origin: ClientURL,
+  },
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+})
+
+const port = 3000
+server.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`)
+})
