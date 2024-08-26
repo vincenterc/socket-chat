13b49c17b0b8ee1f13a3f208d05ddfd553a20dcd
vincenter <vincenterr@yahoo.com.tw>
2024-08-26 17:54:56 +0800

    Style the home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,16 @@
 export default function Page() {
-  return <h1>Hello, world!</h1>
+  return (
+    <>
+      <ul></ul>
+      <form className="bg-[rgb(0,0,0,0.15)] p-1 fixed bottom-0 left-0 right-0 flex h-12 box-border backdrop-blur-[10px]">
+        <input
+          autoComplete="off"
+          className="border-none py-0 px-4 grow rounded-[2rem] m-1 focus:outline-none"
+        />
+        <button className="bg-[#333] border-none py-0 px-4 m-1 rounded-[3px] outline-none text-[#fff]">
+          Send
+        </button>
+      </form>
+    </>
+  )
 }
